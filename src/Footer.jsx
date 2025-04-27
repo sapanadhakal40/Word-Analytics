c4baba9fc0c6a1dce5a03392a616e00c7cfe22c7
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-27 13:25:07 +0300

    Initial project setup with vite and created base components

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; Copyright by BG. All rights reserved.</small>
+      <small>Last checked limits:20 days ago</small>
+    </footer>
+  );
+}
