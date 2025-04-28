e3d923fb731b062cfb7f603806c25fc026e41e91
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-28 09:07:52 +0300

    Create reusable stat components

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,3 +1,18 @@
 export default function Stats() {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <Stat number={0} label="WORDS" />
+      <Stat number={0} label="CHARACTERS" />
+      <Stat number={280} label="INSTAGRAM" />
+      <Stat number={2200} label="FACEBOOK" />
+    </section>
+  );
+}
+function Stat({ number, label }) {
+  return (
+    <section className="stat">
+      <span className="stat__number">{number}</span>
+      <h2 className="second-heading">{label}</h2>
+    </section>
+  );
 }
