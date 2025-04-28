84322746e6a421d916613bfed4e8c4e33672b8c0
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-04-29 01:15:38 +0300

    Calcualte num of words using regex and add social media character counter logic

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,11 +4,19 @@ import { useState } from "react";
 
 export default function Container() {
   const [text, setText] = useState("");
+  const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
   const numberOfCharacters = text.length;
+  const instagramCharactersLeft = 280 - text.length;
+  const facebookCharactersLeft = 2200 - text.length;
   return (
     <main className="container">
       <Textarea text={text} setText={setText} />
-      <Stats numberOfCharacters={numberOfCharacters} />
+      <Stats
+        numberOfWords={numberOfWords}
+        numberOfCharacters={numberOfCharacters}
+        instagramCharactersLeft={instagramCharactersLeft}
+        facebookCharactersLeft={facebookCharactersLeft}
+      />
     </main>
   );
 }
